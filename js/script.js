2e049eb996786bec2c0bee78796fc858bcddba82
shohag390 <shohag390@users.noreply.github.com>
2025-04-01 09:45:16 +0600

    Routing Add

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Routing In JavaScript
+
+const handleHome = () => {
+    window.location = "./index.html"
+}
+
+const handleHtml = () => {
+    window.location = "./htmlCode.html"
+}
+
+const handleCss = () => {
+    window.location = "./cssCode.html"
+}
+
+const handleJavascript = () => {
+    window.location = "./javascript.html"
+}
+
+const handleReact = () => {
+    window.location = "./reactCode.html"
+}
+
+
